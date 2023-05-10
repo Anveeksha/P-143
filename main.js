91e0c631a263f019605b1bb6aa30cc9581cf4b5a
Anveeksha <104507655+Anveeksha@users.noreply.github.com>
2023-05-10 19:17:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 song1="";
 song2="";
+
 song1_status="";
 song2_status="";
 
@@ -10,6 +11,7 @@ leftWristX=0;
 leftWristY=0;
 
 scoreLeftWrist=0;
+scoreRightWrist=0;
 
 function preload(){
     song1=loadSound("Dandelions(PaglaSongs).mp3");
@@ -30,6 +32,14 @@ function draw(){
     image(video,0,0,600,500);
     fill("#FF0000");
     stroke("FF0000");
+    if(scoreRightWrist > 0.2){
+        circle(rightWristX,rightWristY,20);
+        song2.stop();
+            if(song1_status == false){
+                song1.play();
+                document.getElementById("song").innerHTML="Playing-Bones(PaglaSongs).mp3"
+            }
+    }
     if(scoreLeftWrist > 0.2){
         circle(leftWristX,leftWristY,20);
         song1.stop;
@@ -47,11 +57,14 @@ function play(){
 function gotPoses(results){
       if(results.length>0){
         console.log(results);
+        scoreRightWrist=results[0].pose.keypoints[10].score;
         scoreLeftWrist=results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist =" + scoreLeftWrist);
+        console.log("scoreLeftWrist =" + scoreLeftWrist + "scoreRightWrist =" + scoreRightWrist);
+
         rightWristX=results[0].pose.rightWrist.x;
         rightWristY=results[0].pose.rightWrist.y;
         console.log("rightWristX =" + rightWristX + "rightWristY" + rightWristX);
+        
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("leftWristX =" + leftWristX + "leftWristY" + leftWristX);
